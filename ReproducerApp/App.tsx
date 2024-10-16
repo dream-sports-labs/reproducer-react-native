24f27aa36997110b3d876a933b1027b628117cd2
shubhamguptadream11 <shubham.gupta@dream11.com>
2024-10-16 22:08:22 +0530

    fix: app updted

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,36 +1,81 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 
-const App = () => {
+export default function App() {
+  const [showOptions, setShowOptions] = useState(false);
+  const [activeCard, setActiveCard] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.box}>
-        <Text>Margin and Padding Example, Ideally margin of 100 should be applied but only 20 is getting applied</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={[0, 1, 2, 3, 4, 5]}
+        renderItem={({ item, index }) => (
+          <View style={styles.card}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setShowOptions(true);
+                setActiveCard(index);
+              }}
+            >
+              <Text style={styles.buttonText}>Click me</Text>
+            </TouchableOpacity>
+            {showOptions && activeCard === index && (
+              <View style={styles.optionsContainer}></View>
+            )}
+          </View>
+        )}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        contentContainerStyle={styles.listContent}
+      />
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  box: {
+  card: {
     height: 100,
-    backgroundColor: 'lightblue',
-    // Individual margin
-    marginLeft: 20,
-    marginRight: 20,
-    // Aggregate margin (should override marginLeft & marginRight)
-    marginHorizontal: 100,
-    // Individual padding
-    paddingRight: 10,
-    paddingLeft: 10,
-    // Aggregate padding (should override paddingLeft & paddingRight)
-    paddingHorizontal: 100,
+    width: '100%',
+    backgroundColor: 'green',
+    borderRadius: 8,
+    position: 'relative',
+  },
+  button: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  optionsContainer: {
+    backgroundColor: 'yellow',
+    borderRadius: 8,
+    height: 80,
+    width: '50%',
+    position: 'absolute',
+    top: 80,
+    zIndex: 1
+  },
+  separator: {
+    height: 20,
+  },
+  listContent: {
+    padding: 20,
+    paddingBottom: 100,
   },
 });
-
-export default App;
